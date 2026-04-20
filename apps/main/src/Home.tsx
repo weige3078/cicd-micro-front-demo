35cb6ed109bdb100cdaf26537ac53e8684932b0a
1688wdw <1985734033@qq.com>
2026-04-20 16:58:50 +0800

    改了的cicd测试

--- apps/main/src/Home.tsx
+++ apps/main/src/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div style={{ padding: 24 }}>
-      <h1>基座（React）</h1>
+      <h1>基座（React）cicd测试</h1>
       <p>
         <Link to="/vue">进入 Vue 子应用 /vue</Link>
       </p>
